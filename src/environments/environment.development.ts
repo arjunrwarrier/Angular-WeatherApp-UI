5003bd326deed7bf20f0bd5e580d6380cc6dac18
Arjun R Warrier <notwarrier@gmail.com>
2023-03-01 11:34:53 +0530

    Created a simple UI for viewing weather, does not include an API connection. The values are hard coded.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '5f677d2244msh266cd7c458967c0p12c6c5jsne8f106827e10'
+};
